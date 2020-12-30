45291adf29e73ab51e048f5694089553ca2e8466
Jansen Hillis <jansen.hillis@outlook.com>
2020-12-29 17:57:34 -0800

    assignment complete

---
+++ A028-jQuery-DisapearingNinja/images.js
@@ -0,0 +1,15 @@
+
+$('img').click( function(){
+    $(this).hide();
+});
+
+$("button").click( function() {
+    $("#img-1").show();
+    $("#img-2").show();
+    $("#img-3").show();
+    $("#img-4").show();
+    $("#img-5").show();
+    $("#img-6").show();
+    $("#img-7").show();
+    $("#img-8").show();
+});
